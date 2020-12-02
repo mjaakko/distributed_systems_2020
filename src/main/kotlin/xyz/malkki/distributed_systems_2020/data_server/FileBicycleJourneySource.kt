01e633f026e526a104a15cb5e6da9fc5ca1cd4e5
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2020-12-02 23:06:21 +0200

    Rename package

--- src/main/kotlin/xyz/malkki/distributed_systems_2020/data_server/FileBicycleJourneySource.kt
+++ src/main/kotlin/xyz/malkki/distributed_systems_2020/data_server/FileBicycleJourneySource.kt
@@ -1,4 +1,4 @@
-package xyz.malkki.distributed_systems_2020.server
+package xyz.malkki.distributed_systems_2020.data_server
 
 import xyz.malkki.distributed_systems_2020.common.model.BicycleJourney
 import java.io.File
